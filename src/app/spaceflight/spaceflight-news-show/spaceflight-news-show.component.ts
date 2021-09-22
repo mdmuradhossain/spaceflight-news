faf69d123b84f7376ddaf7dc904931406976131e
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-22 19:45:51 +0600

    google-font added

--- src/app/spaceflight/spaceflight-news-show/spaceflight-news-show.component.ts
+++ src/app/spaceflight/spaceflight-news-show/spaceflight-news-show.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { SpaceflightNewsService } from '../spaceflight-news.service';
 
 @Component({
   selector: 'app-spaceflight-news-show',
   templateUrl: './spaceflight-news-show.component.html',
-  styleUrls: ['./spaceflight-news-show.component.css']
+  styleUrls: ['./spaceflight-news-show.component.css'],
 })
 export class SpaceflightNewsShowComponent implements OnInit {
+  articles: any = [];
 
-  constructor() { }
+  constructor(private newsService: SpaceflightNewsService) {}
 
   ngOnInit(): void {
+    this.newsService.getSpaceFlightNews().subscribe((news) => {
+      this.articles = news;
+      console.log(news);
+    });
   }
-
 }
